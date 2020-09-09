cf5af978dc40737bee36aca74f6c1318b08dc0c2
Dan <daniesy@me.com>
2020-09-09 11:48:59 +0300

    Added utc support

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import DateTime from "./components/DateTime.vue";
 import dayjs from "dayjs";
 import localeData from "dayjs/plugin/localeData";
 import isBetween from "dayjs/plugin/isBetween";
+import utc from "dayjs/plugin/utc";
 
 if (!window.dayjs) {    
     window.dayjs = dayjs;
 }
+
 window.dayjs.extend(localeData);
 window.dayjs.extend(isBetween);
+window.dayjs.extend(utc);
 
 require("dayjs/locale/ro");
 require("dayjs/locale/es");
--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import App from "./App.vue";
 import dayjs from "dayjs";
 import localeData from "dayjs/plugin/localeData";
 import isBetween from "dayjs/plugin/isBetween";
-
-dayjs.extend(localeData);
-dayjs.extend(isBetween);
+import utc from "dayjs/plugin/utc";
 
 require("dayjs/locale/ro");
 require("dayjs/locale/es");
 require("dayjs/locale/pt");
 
 window.dayjs = dayjs;
+window.dayjs.extend(localeData);
+window.dayjs.extend(isBetween);
+window.dayjs.extend(utc);
 Vue.config.productionTip = false;
 
 new Vue({
